f50c499fa7ca54be439e2b8f41670b5afb3de8b6
Josh <2015jtw@gmail.com>
2025-01-10 13:51:04 +0100

    Update env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,13 +1,15 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-02";
 
+// SANITY_STUDIO_DATASET for production,  NEXT_PUBLIC_SANITY_DATASET for local dev
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.SANITY_STUDIO_DATASET,
   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
+// SANITY_STUDIO_PROJECT_ID for production, NEXT_PUBLIC_SANITY_PROJECT_ID for local dev
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.SANITY_STUDIO_PROJECT_ID,
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
